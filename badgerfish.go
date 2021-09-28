62b97dc65945ba87f228f8f229774a2587ef487c
a-poor <45295232+a-poor@users.noreply.github.com>
2021-09-28 16:49:03 -0700

    Updating badgerfish and tests

--- badgerfish.go
+++ badgerfish.go
@@ -1,3 +1,72 @@
 package badgerfish
 
+import (
+	"errors"
+	"strings"
+)
 
+var (
+	EscapeCharMap = map[string]string{
+		"<":  "&lt;",
+		">":  "&gt;",
+		"&":  "&amp;",
+		"'":  "&apos;",
+		"\"": "&quot;",
+	}
+	UnEscapeCharMap = map[string]string{
+		"&lt;":   "<",
+		"&gt;":   ">",
+		"&amp;":  "&",
+		"&apos;": "'",
+		"&quot;": "\"",
+	}
+)
+
+var (
+	ErrMalformedXML = errors.New("malformed XML")
+)
+
+type Document struct {
+	Prolog string
+	Root   *Element
+}
+
+func getProlog(data string) string {
+	if len(data) < 2 {
+		return ""
+	}
+
+	if data[0:2] != "<?" {
+		return ""
+	}
+
+	i := strings.Index(data, "?>") + 2
+
+	return data[0:i]
+}
+
+func getRootElement(data string) string {
+	return ""
+}
+
+func ParseDocument(data string) (*Document, error) {
+	clean := strings.TrimSpace(data)
+	prolog := getProlog(clean)
+
+	clean = strings.TrimSpace(clean[len(prolog):])
+
+	doc := Document{Prolog: prolog}
+
+	return &doc, nil
+}
+
+// type Prolog map[string]string
+
+type Element struct {
+	Name           string
+	NameSpace      string
+	Attributes     map[string]string
+	StringContent  []string
+	NestedElements []Element
+	IsEmpty        bool
+}
--- badgerfish_test.go
+++ badgerfish_test.go
@@ -1,3 +1,44 @@
-package badgerfish_test
+package badgerfish
 
+import (
+	"testing"
+)
 
+func TestNothing(t *testing.T) {
+	doc := &Document{}
+	if doc == nil {
+		t.Error("Expected nil document")
+	}
+}
+
+func TestGetProlog(t *testing.T) {
+	testCases := []struct {
+		in     string
+		expect string
+	}{
+		{
+			"",
+			"",
+		},
+		{
+			`<?xml version="1.0" encoding="UTF-8"?>`,
+			`<?xml version="1.0" encoding="UTF-8"?>`,
+		},
+		{
+			`<?xml version="1.0" encoding="UTF-8"?> <hello>world</hello> `,
+			`<?xml version="1.0" encoding="UTF-8"?>`,
+		},
+		{
+			`<?xml version="1.0" encoding="UTF-8"?>
+			<hello>world</hello>`,
+			`<?xml version="1.0" encoding="UTF-8"?>`,
+		},
+	}
+
+	for _, tc := range testCases {
+		res := getProlog(tc.in)
+		if res != tc.expect {
+			t.Errorf("Expected %q, got %q", tc.expect, res)
+		}
+	}
+}
